e2c6edc7d56d7cd455dcd8b2166be54efed4a37e
dezrobbo1 <dezrobbo1@gmail.com>
2026-08-22 06:36:52 +0800

    style: load round-trip harness refinements

--- apps/console/src/main.tsx
+++ apps/console/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { RoundTripTestAppV2 } from "./RoundTripTestAppV2";
 import "./styles.css";
+import "./roundTripTestV2.css";
 
 const roundTripMode = import.meta.env.VITE_SHUTDOWN_TRACKER_ROUND_TRIP_MODE === "true";
 
