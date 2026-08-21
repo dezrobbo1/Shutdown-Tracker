e2aa839a8c92e4e86cbf84e0cf09b07c9a104231
dezrobbo1 <dezrobbo1@gmail.com>
2026-08-22 06:17:18 +0800

    feat: enable round-trip test mode

--- apps/console/src/main.tsx
+++ apps/console/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { RoundTripTestApp } from "./RoundTripTestApp";
 import "./styles.css";
+
+const roundTripMode = import.meta.env.VITE_SHUTDOWN_TRACKER_ROUND_TRIP_MODE === "true";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    {roundTripMode ? <RoundTripTestApp /> : <App />}
   </StrictMode>
 );
