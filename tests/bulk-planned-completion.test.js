506d1dad17c0bce3f68c120b9466a8c81758d134
dezrobbo1 <dezrobbo1@gmail.com>
2026-08-28 17:11:34 +0800

    test: fix bulk intent event imports

--- tests/bulk-planned-completion.test.js
+++ tests/bulk-planned-completion.test.js
@@ -1,5 +1,6 @@
 import test from "node:test";
 import assert from "node:assert/strict";
+import { EXECUTION_EVENT_TYPES } from "../src/execution.js";
 import {
   analyzePlannedCompletionCut,
   buildBulkCompletionExecutionIntent,
